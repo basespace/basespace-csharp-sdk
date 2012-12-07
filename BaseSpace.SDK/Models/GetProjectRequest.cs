217d553191c6001c76381d4e27df98cec1d522ee
sucheth koppa <skoppa@illumina.com>
2012-12-06 18:00:16 -0800

    Added methods for Run, Project, attempt to build model for list request.
    Added methods for Run, Project, attempt to build model for list request.

--- BaseSpace.SDK/Models/GetProjectRequest.cs
+++ BaseSpace.SDK/Models/GetProjectRequest.cs
@@ -5,22 +5,22 @@ using System.Text;
 
 namespace Illumina.BaseSpace.SDK.Models
 {
-    public class GetUserRequest : IBaseSpaceRequest
+    public class GetProjectRequest : IBaseSpaceRequest
     {
         /// <summary>
-        /// Get basic user infomation
+        /// Get a specific project
         /// </summary>
-        /// <param name="id">User Id or null for current user</param>
-        public GetUserRequest(string id)
+        /// <param name="id">Project Id</param>
+        public GetProjectRequest(string id)
         {
             Id = id;
         }
-        public string Id { get; set; }
 
+        public string Id { get; set; }
 
         public string GenerateUrl(string version)
         {
-            return string.Format("{0}/users/{1}", version, Id ?? "current");
+            return string.Format("{0}/projects/{1}", version, Id);
         }
     }
 }
--- BaseSpace.SDK/Models/GetRunRequest.cs
+++ BaseSpace.SDK/Models/GetRunRequest.cs
@@ -5,22 +5,22 @@ using System.Text;
 
 namespace Illumina.BaseSpace.SDK.Models
 {
-    public class GetUserRequest : IBaseSpaceRequest
+    public class GetRunRequest
     {
         /// <summary>
-        /// Get basic user infomation
+        /// Get specific run
         /// </summary>
-        /// <param name="id">User Id or null for current user</param>
-        public GetUserRequest(string id)
+        /// <param name="id">Run Id</param>
+        public GetRunRequest(string id)
         {
             Id = id;
         }
-        public string Id { get; set; }
 
+        public string Id { get; set; }
 
         public string GenerateUrl(string version)
         {
-            return string.Format("{0}/users/{1}", version, Id ?? "current");
+            return string.Format("{0}/runs/{1}", version, Id);
         }
     }
 }
