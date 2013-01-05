7f4196dd0c2365c87d26f91b8865ae22f2097a6a
Sucheth Koppa <skoppa@illumina.com>
2013-01-04 17:23:20 -0800

    Request/Response files for AppSession methods.

---
+++ BaseSpace.SDK/ServiceModels/Request/GetAppSessionRequest.cs
@@ -0,0 +1,16 @@
+﻿namespace Illumina.BaseSpace.SDK.ServiceModels
+{
+    public class GetAppSessionRequest
+    {
+        /// <summary>
+        /// Get specific AppResult
+        /// </summary>
+        /// <param name="id">AppSession Id</param>
+        public GetAppSessionRequest(string id)
+        {
+            Id = id;
+        }
+
+        public string Id { get; set; }
+    }
+}
