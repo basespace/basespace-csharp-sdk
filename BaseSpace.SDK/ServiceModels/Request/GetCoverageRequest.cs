f35966907eba4c8a9fce41e612a545edad7711c0
Sucheth Koppa <skoppa@illumina.com>
2013-01-18 11:36:42 -0800

    Added coverage methods.

---
+++ BaseSpace.SDK/ServiceModels/Request/GetCoverageRequest.cs
@@ -0,0 +1,25 @@
+﻿namespace Illumina.BaseSpace.SDK.ServiceModels
+{
+    public class GetCoverageRequest
+    {
+        /// <summary>
+        /// Get Coverage by Id and Chromosome
+        /// </summary>
+        /// <param name="id">File Id</param>
+        /// <param name="chrom">Chromosome</param>
+        /// <param name="startPos">Chromosome start position</param>
+        /// <param name="endPos">Chromosome end position</param>
+        public GetCoverageRequest(string id, string chrom, string startPos, string endPos)
+        {
+            Id = id;
+            Chrom = chrom;
+            StartPos = startPos;
+            EndPos = endPos;
+        }
+
+        public string Id { get; set; }
+        public string Chrom { get; set; }
+        public string StartPos { get; set; }
+        public string EndPos { get; set; }
+    }
+}
---
+++ BaseSpace.SDK/Types/Coverage.cs
@@ -0,0 +1,38 @@
+﻿using System.Runtime.Serialization;
+
+namespace Illumina.BaseSpace.SDK.Types
+{
+    [DataContract(Name = "Coverage")]
+    public class Coverage
+    {
+        [DataMember(IsRequired = true)]
+        public string Id { get; set; }
+
+        [DataMember]
+        public long[] MeanCoverage { get; set; }
+        [DataMember]
+        public string Chrom { get; set; }
+        
+        [DataMember]
+        public long StartPos { get; set; }
+        
+        [DataMember]
+        public long EndPos { get; set; }
+        
+        [DataMember]
+        public int BucketSize { get; set; }
+    }
+
+    [DataContract(Name = "CoverageMeta")]
+    public class CoverageMeta
+    {
+        [DataMember(IsRequired = true)]
+        public string Id { get; set; }
+        
+        [DataMember]
+        public long MaxCoverage { get; set; }
+
+        [DataMember]
+        public int CoverageGranularity { get; set; }
+    }
+}
