72e8124c8d11c660f3391fa351902c6ec6b33af7
Patrick Bird <patbird@gmail.com>
2013-02-13 13:46:50 -0800

    Added IAuthentication.cs to the repository.

---
+++ BaseSpace.SDK/Interfaces/IAuthentication.cs
@@ -0,0 +1,25 @@
+﻿using System.Net;
+
+namespace Illumina.BaseSpace.SDK
+{
+	internal interface IAuthentication
+	{
+		void UpdateHttpHeader(HttpWebRequest request);
+	}
+
+	internal class OAuth1Authentication : IAuthentication
+	{
+		public void UpdateHttpHeader(HttpWebRequest request)
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+
+	internal class OAuth2Authentication : IAuthentication
+	{
+		public void UpdateHttpHeader(HttpWebRequest request)
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+}
