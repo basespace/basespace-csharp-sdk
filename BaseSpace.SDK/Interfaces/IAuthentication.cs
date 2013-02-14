b40541e4071ef4fea7e44890a66eececa46d462a
Patrick Bird <patbird@gmail.com>
2013-02-13 16:37:57 -0800

    Added IAuthentication property to BaseSpaceClientSettings.

--- BaseSpace.SDK/Interfaces/IAuthentication.cs
+++ BaseSpace.SDK/Interfaces/IAuthentication.cs
@@ -2,24 +2,50 @@
 
 namespace Illumina.BaseSpace.SDK
 {
-	internal interface IAuthentication
+	public interface IAuthentication
 	{
 		void UpdateHttpHeader(HttpWebRequest request);
 	}
 
-	internal class OAuth1Authentication : IAuthentication
+	public class OAuth1Authentication : IAuthentication
 	{
+		public OAuth1Authentication(string username, string password)
+		{
+			
+		}
+
+
 		public void UpdateHttpHeader(HttpWebRequest request)
 		{
 			throw new System.NotImplementedException();
 		}
 	}
 
-	internal class OAuth2Authentication : IAuthentication
+	public class OAuth2Authentication : IAuthentication
 	{
+		public OAuth2Authentication(string appId, string appSecret, string token)
+		{
+			
+		}
+
 		public void UpdateHttpHeader(HttpWebRequest request)
 		{
 			throw new System.NotImplementedException();
 		}
 	}
+
+	//public class AuthenticationBridge
+	//{
+	//	public AuthenticationBridge(string username, string password)
+	//	{
+	//		Authentication = new OAuth1Authentication(username, password);
+	//	}
+
+	//	public AuthenticationBridge(string appId, string appSecret, string token)
+	//	{
+	//		Authentication = new OAuth2Authentication(appId, appSecret, token);
+	//	}
+
+	//	internal IAuthentication Authentication { get; set; }
+	//}
 }
--- BaseSpace.SDK/Interfaces/IClientSettings.cs
+++ BaseSpace.SDK/Interfaces/IClientSettings.cs
@@ -1,21 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace Illumina.BaseSpace.SDK
+﻿namespace Illumina.BaseSpace.SDK
 {
-    public interface IClientSettings
-    {
-        uint RetryAttempts { get; }
-        string AppClientId { get; }
-        string AppClientSecret { get; }
-        string BaseSpaceWebsiteUrl { get; }
-        string BaseSpaceApiUrl { get; }
-        string AppRedirectUrl { get; }
-        string Version { get; }
-        string OauthAuthorizeUrl { get; }
-        uint FileMultipartSizeThreshold { get; }
-
-    }
+	public interface IClientSettings
+	{
+		uint RetryAttempts { get; }
+		string AppClientId { get; }
+		string AppClientSecret { get; }
+		string BaseSpaceWebsiteUrl { get; }
+		string BaseSpaceApiUrl { get; }
+		string AppRedirectUrl { get; }
+		string Version { get; }
+		string OauthAuthorizeUrl { get; }
+		uint FileMultipartSizeThreshold { get; }
+		IAuthentication Authentication { get; }
+	}
 }
