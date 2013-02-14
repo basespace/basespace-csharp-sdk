9b4d79d768579968998e7f4804038177532ae60b
Patrick Bird <patbird@gmail.com>
2013-02-14 15:57:39 -0800

    Moved the OAuth2Authentication class into its own file.

---
+++ BaseSpace.SDK/Infrastructure/OAuth2Authentication.cs
@@ -0,0 +1,40 @@
+﻿using System.Net;
+
+namespace Illumina.BaseSpace.SDK
+{
+	internal class OAuth2Authentication : IAuthentication
+	{
+		private readonly string authCode;
+
+		public OAuth2Authentication(string appId, string appSecret)
+		{
+			AppId = appId;
+			AppSecret = appSecret;
+		}
+
+		internal OAuth2Authentication(string authCode)
+		{
+			this.authCode = authCode;
+		}
+
+		public string AppId { get; private set; }
+
+		public string AppSecret { get; private set; }
+
+		public void UpdateHttpHeader(HttpWebRequest request, IRequestOptions requestOptions)
+		{
+			if (requestOptions != null)
+			{
+				UpdateHttpHeader(request.Headers);
+			}
+		}
+
+		internal void UpdateHttpHeader(WebHeaderCollection headers)
+		{
+			if (headers != null && !string.IsNullOrEmpty(authCode))
+			{
+				headers.Add("Authorization", string.Format("Bearer {0}", authCode));
+			}
+		}
+	}
+}
--- BaseSpace.SDK/Interfaces/IAuthentication.cs
+++ BaseSpace.SDK/Interfaces/IAuthentication.cs
@@ -6,40 +6,4 @@ namespace Illumina.BaseSpace.SDK
 	{
 		void UpdateHttpHeader(HttpWebRequest request, IRequestOptions requestOptions);
 	}
-
-	internal class OAuth2Authentication : IAuthentication
-	{
-		private string authCode;
-
-		public OAuth2Authentication(string appId, string appSecret)
-		{
-			AppId = appId;
-			AppSecret = appSecret;
-		}
-
-		internal OAuth2Authentication(string authCode)
-		{
-			authCode = authCode;
-		}
-
-		public string AppId { get; private set; }
-
-		public string AppSecret { get; private set; }
-
-		public void UpdateHttpHeader(HttpWebRequest request, IRequestOptions requestOptions)
-		{
-			if (requestOptions != null)
-			{
-				UpdateHttpHeader(request.Headers);
-			}
-		}
-
-		internal void UpdateHttpHeader(WebHeaderCollection headers)
-		{
-			if (headers != null && !string.IsNullOrEmpty(authCode))
-			{
-				headers.Add("Authorization", string.Format("Bearer {0}", authCode));
-			}
-		}
-	}
 }
