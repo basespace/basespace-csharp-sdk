59d82808117700ab71e1944d5f09a109d8426931
Patrick Bird <patbird@gmail.com>
2013-02-19 13:35:31 -0800

    Removed lock functionality from ListRunsRequest.cs.

--- BaseSpace.SDK/ServiceModels/Request/ListRunsRequest.cs
+++ BaseSpace.SDK/ServiceModels/Request/ListRunsRequest.cs
@@ -5,9 +5,5 @@ namespace Illumina.BaseSpace.SDK.ServiceModels
     public class ListRunsRequest : AbstractResourceListRequest<RunSortByParameters>
     {
 		public string Statuses { get; set; }
-
-		public bool IncludeLocked { get; set; }
-
-		public string ValidInstrumentType { get; set; }
     }
 }
