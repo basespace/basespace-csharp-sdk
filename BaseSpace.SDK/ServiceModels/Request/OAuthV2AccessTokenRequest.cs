8fe24e6058604e8b674926f5cc6543c0118fc58d
Gery Vessere <gvessere@illumina.com>
2013-02-20 09:23:42 -0800

    add missing files from last checkin

---
+++ BaseSpace.SDK/ServiceModels/Request/OAuthV2AccessTokenRequest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace Illumina.BaseSpace.SDK.ServiceModels.Request
+{
+
+    [DataContract]
+    public class OAuthV2AccessTokenRequest
+    {
+        public OAuthV2AccessTokenRequest(string clientId, string clientSecret, string redirectUri, string authorizationCode)
+        {
+            GrantType = "authorization_code";
+            ClientId = clientId;
+            ClientSecret = clientSecret;
+            RedirectUri = redirectUri;
+            AuthorizationCode = authorizationCode;
+        }
+
+        [DataMember(Name= "grant_type")]
+        public string GrantType { get; set; }
+
+        [DataMember(Name = "code")]
+        public string AuthorizationCode { get; set; }
+
+        [DataMember(Name = "redirect_uri")]
+        public string RedirectUri { get; set; }
+
+        [DataMember(Name = "client_id")]
+        public string ClientId { get; set; }
+
+        [DataMember(Name = "client_secret")]
+        public string ClientSecret { get; set; }
+    }
+
+}
