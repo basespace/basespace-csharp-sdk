0a96816fd751053529351f0de8ed61c487834645
Gery Vessere <gvessere@illumina.com>
2013-02-25 15:20:36 -0800

    roll back app session update change and fix in the api

--- BaseSpace.SDK/ServiceModels/Request/UpdateAppSessionRequest.cs
+++ BaseSpace.SDK/ServiceModels/Request/UpdateAppSessionRequest.cs
@@ -9,17 +9,13 @@ namespace Illumina.BaseSpace.SDK.ServiceModels
         /// </summary>
         /// <param name="id">AppSession Id</param>
         /// <param name="status">AppSession Status</param>
-        public UpdateAppSessionRequest(string id, string status, ApplicationCompact application, UserCompact user)
+        public UpdateAppSessionRequest(string id, string status)
         {
             Id = id;
             Status = status;
-            Application = application;
-            UserCreatedBy = user;
         }
 
         public string Status { get; set; }
         public string StatusSummary { get; set; }
-        public ApplicationCompact Application { get; set; }
-        public UserCompact UserCreatedBy { get; set; }
     }
 }
