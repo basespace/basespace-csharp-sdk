48ff56e2f174c4ee09e65b11b9156f4b5b748ba0
Patrick Bird <patbird@gmail.com>
2013-02-25 11:34:59 -0800

    Put AbstractRequest into its own file.

---
+++ BaseSpace.SDK/ServiceModels/Request/AbstractRequest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using ServiceStack.ServiceClient.Web;
+
+namespace Illumina.BaseSpace.SDK.ServiceModels
+{
+	public abstract class AbstractRequest<TReturn>
+		where TReturn : class
+	{
+		protected AbstractRequest()
+		{
+			HttpMethod = HttpMethods.GET;
+		}
+
+		protected HttpMethods HttpMethod { get; set; }
+
+		protected string Version
+		{
+			get { return "v1pre3"; }
+		}
+
+		internal Func<TReturn> GetFunc(ServiceClientBase client, IRequestOptions options)
+		{
+			var httpMethod = HttpMethod.ToString();
+
+			return () => client.Send<TReturn>(httpMethod, GetUrl(), (httpMethod == "GET") ? null : this);
+		}
+
+		internal string GetName()
+		{
+			return String.Format("{0} request to {1} ", HttpMethod, GetUrl());
+		}
+
+		protected abstract string GetUrl();
+	}
+}
--- BaseSpace.SDK/ServiceModels/Request/AbstractResourceRequest.cs
+++ BaseSpace.SDK/ServiceModels/Request/AbstractResourceRequest.cs
@@ -1,38 +1,5 @@
-﻿using System;
-using ServiceStack.ServiceClient.Web;
-
-namespace Illumina.BaseSpace.SDK.ServiceModels
+﻿namespace Illumina.BaseSpace.SDK.ServiceModels
 {
-	public abstract class AbstractRequest<TReturn>
-		where TReturn : class
-	{
-		protected AbstractRequest()
-		{
-			HttpMethod = HttpMethods.GET;
-		}
-
-		protected HttpMethods HttpMethod { get; set; }
-
-		protected string Version
-		{
-			get { return "v1pre3"; }
-		}
-
-		internal Func<TReturn> GetFunc(JsonServiceClient client, IRequestOptions options)
-		{
-			var httpMethod = HttpMethod.ToString();
-
-			return () => client.Send<TReturn>(httpMethod, GetUrl(), (httpMethod == "GET") ? null : this);
-		}
-
-		internal string GetName()
-		{
-			return String.Format("{0} request to {1} ", HttpMethod, GetUrl());
-		}
-
-		protected abstract string GetUrl();
-	}
-
 	public abstract class AbstractResourceRequest<TReturn> : AbstractRequest<TReturn> 
 		where TReturn : class
     {
