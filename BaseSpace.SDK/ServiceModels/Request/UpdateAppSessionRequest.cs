a0f3e87d8402fc58e819bda9ec7244da0993ef9a
Gery Vessere <gvessere@illumina.com>
2013-02-25 12:51:10 -0800

    Add needed properties for appsession update call

--- BaseSpace.SDK/ServiceModels/Request/UpdateAppSessionRequest.cs
+++ BaseSpace.SDK/ServiceModels/Request/UpdateAppSessionRequest.cs
@@ -1,4 +1,6 @@
-﻿namespace Illumina.BaseSpace.SDK.ServiceModels
+﻿using Illumina.BaseSpace.SDK.Types;
+
+namespace Illumina.BaseSpace.SDK.ServiceModels
 {
     public class UpdateAppSessionRequest : AbstractResourceRequest
     {
@@ -7,13 +9,17 @@
         /// </summary>
         /// <param name="id">AppSession Id</param>
         /// <param name="status">AppSession Status</param>
-        public UpdateAppSessionRequest(string id, string status)
+        public UpdateAppSessionRequest(string id, string status, ApplicationCompact application, UserCompact user)
         {
             Id = id;
             Status = status;
+            Application = application;
+            UserCreatedBy = user;
         }
 
         public string Status { get; set; }
         public string StatusSummary { get; set; }
+        public ApplicationCompact Application { get; set; }
+        public UserCompact UserCreatedBy { get; set; }
     }
 }
