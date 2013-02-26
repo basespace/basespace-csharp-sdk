494a097224137b8ac707c3f5986a3940af4814dd
Patrick Bird <patbird@gmail.com>
2013-02-26 10:43:36 -0800

    Removed redundant property on FileRequestPost.cs.

--- BaseSpace.SDK/ServiceModels/Request/FileRequestPost.cs
+++ BaseSpace.SDK/ServiceModels/Request/FileRequestPost.cs
@@ -4,8 +4,6 @@ namespace Illumina.BaseSpace.SDK.ServiceModels
 {
     public class FileRequestPost : AbstractResourceRequest<FileResponse>
     {
-	    public string Id { get; set; }
-
 	    public FileUploadStatus? UploadStatus { get; set; }
 
 		protected override string GetUrl()
