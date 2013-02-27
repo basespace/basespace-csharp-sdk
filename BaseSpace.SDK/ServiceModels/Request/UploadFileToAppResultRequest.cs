98c3019232430ab3b5df62f1287688f9e24b5137
Patrick Bird <patbird@gmail.com>
2013-02-26 16:17:54 -0800

    Removed redundant code.

--- BaseSpace.SDK/ServiceModels/Request/UploadFileToAppResultRequest.cs
+++ BaseSpace.SDK/ServiceModels/Request/UploadFileToAppResultRequest.cs
@@ -2,20 +2,10 @@
 {
     public class UploadFileToAppResultRequest : FileUploadRequestBase<UploadFileToAppResultResponse>
     {
-        public UploadFileToAppResultRequest(string resourceId, string name, string directory = null, string resourceIdentifierInUri = "appresults")
-			: base(resourceId, name, directory, resourceIdentifierInUri)
+        public UploadFileToAppResultRequest(string resourceId, string name, string directory = null)
+            : base(resourceId, name, directory, "appresults")
         {
         }
-
-        public UploadFileToAppResultRequest()
-			: this(null, null)
-        {
-        }
-
-		protected override string GetUrl()
-		{
-			return string.Format("{0}/{1}/{2}/files", Version, ResourceIdentifierInUri, Id);
-		}
 	}
 }
 
