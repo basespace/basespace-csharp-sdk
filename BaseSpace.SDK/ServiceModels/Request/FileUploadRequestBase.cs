673060d8cca7b54e5963746ee4054193ca1d4896
Patrick Bird <patbird@gmail.com>
2013-02-26 16:17:19 -0800

    Implemented GetFunc() and GetUrl() in FileUploadRequestBase.cs.

--- BaseSpace.SDK/ServiceModels/Request/FileUploadRequestBase.cs
+++ BaseSpace.SDK/ServiceModels/Request/FileUploadRequestBase.cs
@@ -1,13 +1,21 @@
-﻿
+﻿using System;
+using System.IO;
+using ServiceStack.ServiceClient.Web;
+
 namespace Illumina.BaseSpace.SDK.ServiceModels
 {
 	public abstract class FileUploadRequestBase<TResult> : AbstractResourceRequest<TResult>
 		where TResult : class
 	{
+	    private readonly FileInfo fileInfo;
+
 		protected FileUploadRequestBase(string id, string name, string directory, string resourceIdentifierInUri)
 		{
+            HttpMethod = HttpMethods.PUT;
+
 			Id = id;
 			Name = name;
+		    fileInfo = new FileInfo(name);
 
 			if (directory != null)
 				Directory = directory;
@@ -22,5 +30,15 @@ namespace Illumina.BaseSpace.SDK.ServiceModels
 		public bool? MultiPart { get; set; }
 
 		public string ResourceIdentifierInUri { get; set; }
+
+        internal override Func<TResult> GetFunc(ServiceClientBase client)
+        {
+            return () => client.PostFileWithRequest<TResult>(GetUrl(), fileInfo, this);
+        }
+
+        protected override string GetUrl()
+        {
+            return string.Format("{0}/{1}/{2}/files", Version, ResourceIdentifierInUri, Id);
+        }
 	}
 }
