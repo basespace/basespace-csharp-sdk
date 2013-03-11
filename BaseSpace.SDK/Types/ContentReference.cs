3f2ddccd047bd43a0c2c369f275078f36a7c601c
Gery Vessere <gvessere@illumina.com>
2013-03-11 12:04:07 -0700

    add constructor to ContentReference enables setting the href fields to default settings to avoid api errors

--- BaseSpace.SDK/Types/ContentReference.cs
+++ BaseSpace.SDK/Types/ContentReference.cs
@@ -7,7 +7,18 @@ namespace Illumina.BaseSpace.SDK.Types
 	public class ContentReference<T> : IContentReference<T>
 		where T : AbstractResource
 	{
-		[DataMember]
+
+        public ContentReference(T resource, string relation)
+        {
+            Href = resource.Href;
+            HrefContent = resource.Href;
+            Rel = relation;
+            Content = resource;
+            Type = typeof(T).ToString().Replace("Compact", "").Replace("Illumina.BaseSpace.SDK.Types.", "");
+        }
+            
+            
+        [DataMember]
 		public string Rel { get; set; }
 
 		[DataMember]
