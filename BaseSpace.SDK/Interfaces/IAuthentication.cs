e1ba8fb29ed4eb7832c09d0a9ee91dc3b92b4d0e
Jessica Gordon <jygordon@illumina.com>
2013-03-22 14:50:13 -0700

    support oauth1 for multi part upload

--- BaseSpace.SDK/Interfaces/IAuthentication.cs
+++ BaseSpace.SDK/Interfaces/IAuthentication.cs
@@ -1,9 +1,11 @@
-﻿using System.Net;
+﻿using System;
+using System.Net;
 
 namespace Illumina.BaseSpace.SDK
 {
 	public interface IAuthentication
 	{
 		void UpdateHttpHeader(HttpWebRequest request);
+	    void UpdateHttpHeader(WebHeaderCollection headers, Uri requestUri, string requestMethod);
 	}
 }
