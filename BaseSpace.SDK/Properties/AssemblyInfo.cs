b08ecdde919a9f97ad00bd3986f75ec4e2efbfe0
gvessere <gvessere@illumina.com>
2013-05-01 13:38:48 -0700

    fix up nuget package details

--- BaseSpace.SDK/Properties/AssemblyInfo.cs
+++ BaseSpace.SDK/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Illumina")]
 [assembly: AssemblyProduct("Illumina.BaseSpace.SDK")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCopyright("Copyright © Illumina inc. 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
