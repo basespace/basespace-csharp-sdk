8344f84b009d5dd4d1d65e63b7461718641853cb
Sucheth Koppa <skoppa@illumina.com>
2013-05-13 16:43:42 -0700

    Added method to get API meta data.

---
+++ BaseSpace.SDK/ServiceModels/Request/GetApiMetaRequest.cs
@@ -0,0 +1,17 @@
+﻿using Illumina.BaseSpace.SDK.Types;
+
+namespace Illumina.BaseSpace.SDK.ServiceModels
+{
+    public class GetApiMeta : AbstractResourceRequest<ApiMeta>
+    {
+        protected override string GetUrl()
+        {
+            return string.Format("{0}/meta", Version);
+        }
+
+        internal override string GetLogMessage()
+        {
+            return "";
+        }
+    }
+}
