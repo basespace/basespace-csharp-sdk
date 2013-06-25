48cf472c86d724000f4ba24519501a39fc00495e
Gery Vessere <gvessere@illumina.com>
2013-06-24 18:26:35 -0700

    return resource in extension methods

--- BaseSpace.SDK/Extensions/IResourceExtensions.cs
+++ BaseSpace.SDK/Extensions/IResourceExtensions.cs
@@ -26,16 +26,16 @@ namespace Illumina.BaseSpace.SDK.Extensions
             return refs.ToDictionary(r => r.Name, r => r.Content);
         }
 
-        public static IEnumerable<KeyValuePair<string, T>> OfTypeEntityWithName<T>(this IResource[] references) where T : IAbstractResource
+        public static IEnumerable<KeyValuePair<string, IResource>> OfTypeEntityWithName<T>(this IResource[] references) where T : IAbstractResource
         {
             var refs = references.ReferencesOfRefType<T>();
             if (refs == null)
                 return null;
 
-            return refs.Select(r => new KeyValuePair<string, T>(r.Name, r.Content));
+            return refs.Select(r => new KeyValuePair<string, IResource>(r.Name, r));
         }
 
-        public static IEnumerable<T> OfTypeEntity<T>(this IResource[] references) where T : IAbstractResource
+        public static IEnumerable<IResource> OfTypeEntity<T>(this IResource[] references) where T: IAbstractResource
         {
             return references.OfTypeEntityWithName<T>().Select(kvp => kvp.Value);
         }
