fa29886302f65b6c79d0381d4749ded8bbeb1aee
Tony Liu <tliu1@illumina.com>
2014-11-24 15:18:10 -0800

    listlibrarycontainers

---
+++ BaseSpace.SDK/ServiceModels/Request/ListLibraryContainersRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Illumina.BaseSpace.SDK.Types;
+using System.ComponentModel;
+
+namespace Illumina.BaseSpace.SDK.ServiceModels
+{
+    public class ListLibraryContainersRequest : AbstractResourceListRequest<ListLibraryContainersResponse, LibraryContainerSortFields>
+    {
+        [Description("The PrepRunId to filter by.")]
+		public string PrepRunId { get; set; }
+
+        protected override string GetUrl()
+        {
+            return String.Format("{0}/users/current/librarycontainers", Version);
+        }
+	}
+}
