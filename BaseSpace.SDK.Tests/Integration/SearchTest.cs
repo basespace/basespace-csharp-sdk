c0c73f9ad725bc47d0ee0f8e88674dcfd3a6c858
Van Le-Pham <vlepham@illumina.com>
2015-08-03 14:54:00 -0700

    Add some more returned properties in SearchResult

---
+++ BaseSpace.SDK.Tests/Integration/SearchTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Illumina.BaseSpace.SDK.ServiceModels;
+using Illumina.BaseSpace.SDK.Types;
+using Xunit;
+
+namespace Illumina.BaseSpace.SDK.Tests.Integration
+{
+    public class SearchTest : BaseIntegrationTest
+    {
+        [Fact]
+        public void SearchApiTest()
+        {
+            //Assume you have project BaseSpaceDemo in your account
+            var searchRequest = new SearchRequest(query: "project.name:BaseSpaceDemo", scope: "appresult_files");
+            var response = Client.Search(searchRequest);
+
+            Assert.NotNull(response); 
+            Assert.NotNull(response.Response);
+            Assert.NotNull(response.Response.Items);
+        }
+    }
+}
